4a2d180c53aa13fc0de7208ad0c5ecb3ee8dfc84
meadery <799638965@qq.com>
2023-04-11 01:45:44 +0800

    feat: v2

--- src/components/Tree/index.tsx
+++ src/components/Tree/index.tsx
@@ -75,6 +75,8 @@ const LayoutTree: React.FC = () => {
     useEffect(() => {
         const gData = formatToTreeNode(_.cloneDeep(tree))
         setGData(gData)
+        // @ts-ignore
+        setExpandedKeys(getAllkeys(tree))
     }, [tree])
 
 
@@ -107,6 +109,8 @@ const LayoutTree: React.FC = () => {
             curItem
         ]
         setGData(data)
+        // @ts-ignore
+        setExpandedKeys(data)
         const treeData = formatToTreeData(_.cloneDeep(data))
         store.dispatch({
             type: 'updateLayoutTree',
@@ -118,12 +122,9 @@ const LayoutTree: React.FC = () => {
 
     function initGData() {
         if (gData.length) return;
-        if (tree.length) {
-            const gData = formatToTreeNode(tree)
-            console.log('gData', gData)
-            return setGData(gData)
+        if (!tree.length) {
+            createNewTree()
         }
-        createNewTree()
     }
 
     useEffect(() => {
@@ -199,74 +200,6 @@ const LayoutTree: React.FC = () => {
         })
     }
 
-    const onDragEnter: TreeProps['onDragEnter'] = info => {
-        // expandedKeys 需要受控时设置
-        // setExpandedKeys(info.expandedKeys)
-    };
-
-    const onDrop: TreeProps['onDrop'] = info => {
-        const dropKey = info.node.key;
-        const dragKey = info.dragNode.key;
-        const dropPos = info.node.pos.split('-');
-        const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
-
-        const loop = (
-            data: DataNode[],
-            key: React.Key,
-            callback: (node: DataNode, i: number, data: DataNode[]) => void,
-        ) => {
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].key === key) {
-                    return callback(data[i], i, data);
-                }
-                if (data[i].children) {
-                    loop(data[i].children!, key, callback);
-                }
-            }
-        };
-        const data = [...gData];
-
-        // Find dragObject
-        let dragObj: DataNode;
-        loop(data, dragKey, (item, index, arr) => {
-            arr.splice(index, 1);
-            dragObj = item;
-        });
-
-        if (!info.dropToGap) {
-            // Drop on the content
-            loop(data, dropKey, item => {
-                item.children = item.children || [];
-                // where to insert 示例添加到头部，可以是随意位置
-                item.children.unshift(dragObj);
-            });
-        } else if (
-            ((info.node as any).props.children || []).length > 0 && // Has children
-            (info.node as any).props.expanded && // Is expanded
-            dropPosition === 1 // On the bottom gap
-        ) {
-            loop(data, dropKey, item => {
-                item.children = item.children || [];
-                // where to insert 示例添加到头部，可以是随意位置
-                item.children.unshift(dragObj);
-                // in previous version, we use item.children.push(dragObj) to insert the
-                // item to the tail of the children
-            });
-        } else {
-            let ar: DataNode[] = [];
-            let i: number;
-            loop(data, dropKey, (_item, index, arr) => {
-                ar = arr;
-                i = index;
-            });
-            if (dropPosition === -1) {
-                ar.splice(i!, 0, dragObj!);
-            } else {
-                ar.splice(i! + 1, 0, dragObj!);
-            }
-        }
-        setGData(_.cloneDeep(data));
-    };
 
     // 新增节点的表单数据
     const [form] = Form.useForm();
@@ -484,8 +417,6 @@ const LayoutTree: React.FC = () => {
                 defaultExpandAll={true}
                 expandedKeys={expandedKeys}
                 blockNode
-                onDragEnter={onDragEnter}
-                onDrop={onDrop}
                 treeData={gData}
                 selectedKeys={[currentKey]}
             />
